df40418d66e22b616614830142c6745bb03ba5e3
ivanreimer <ivanreimer1@gmail.com>
2016-07-15 22:00:49 -0300

    Ultimo dia antes de las vacaciones

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -3,9 +3,13 @@ function Mostrar()
 
 	var contador=0;
 	var acumulador=0;
-
-
-
+	var numero;
+	while(contador<5)
+	{
+		contador=contador+1;
+		numero=prompt("Ingrese numero","0");
+		acumulador=acumulador+parseInt(numero);
+	}
 document.getElementById('suma').value=acumulador;
 document.getElementById('promedio').value=acumulador/5;
 
