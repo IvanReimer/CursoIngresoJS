f192ea2562b7a177643948f505e368dea02e21b8
ivanreimer <ivanreimer1@gmail.com>
2016-07-15 21:12:41 -0300

    while

--- 5-InstruccionWhile/jsIteraciones (1).js
+++ 5-InstruccionWhile/jsIteraciones (1).js
@@ -1,6 +1,21 @@
 function Mostrar()
 {
 	alert('iteración while');
-
+var contador=0;
+/*while(contador<=5)*/
+while(true)
+{
+	contador=contador+1;
+	if(contador==3)
+	{
+		continue;
+	}
+	alert("while"+contador)
+	if(contador==6)
+	{
+		break;
+	}
+	alert("fin"+contador);
+}
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -1,7 +1,10 @@
 function Mostrar()
 {
 
-var clave = prompt("ingrese el número clave.");
-
-
+var clave;
+while(clave!="utn750")
+{
+	clave = prompt("ingrese el número clave.");	
+}
+alert("Ingreso con exito la clave");
 }//FIN DE LA FUNCIÓN
