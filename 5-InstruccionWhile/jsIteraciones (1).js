e847db99dec479ed5cab699c70fef55b241b33b0
IvanReimer <ivanreimer1@gmail.com>
2016-07-17 07:35:47 -0300

    while!

--- 5-InstruccionWhile/jsIteraciones (1).js
+++ 5-InstruccionWhile/jsIteraciones (1).js
@@ -3,7 +3,7 @@ function Mostrar()
 	alert('iteración while');
 var contador=0;
 /*while(contador<=5)*/
-while(true)
+/*while(true)
 {
 	contador=contador+1;
 	if(contador==3)
@@ -16,6 +16,10 @@ while(true)
 		break;
 	}
 	alert("fin"+contador);
-}
-
+}//fin funcion while*/
+while(contador<10)
+{
+	contador++;
+	alert(contador);
+}//FIN WHILE
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -4,8 +4,12 @@ function Mostrar()
 	var contador=0;
 	var acumulador=0;
 	var respuesta='si';
-
-
+while()
+{
+	contador++;
+	contador=prompt("Ingrese numero");
+	acumulador=acumulador+parseInt(contador);
+}
 document.getElementById('suma').value=acumulador;
 document.getElementById('promedio').value=acumulador/contador;
 
